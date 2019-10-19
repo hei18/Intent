2025c58e52cecae2184499e8e9987682e9262273
hei18 <gilangtria18@gmail.com>
2019-10-19 23:20:41 +0700

    Initial commit

---
+++ app/src/main/java/com/evilcorp/intent/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.evilcorp.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val id:Int = 10
+    val language:String = "kotlin"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_klik.setOnClickListener(){
+            intent = Intent(this, ActivityKedua::class.java)
+            intent.putExtra("id_value", id)
+            intent.putExtra("language_value", language)
+            startActivity(intent)
+        }
+
+
+
+
+    }
+}
